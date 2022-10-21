cc6093f7811f882bc7ebade490fc1f3c0094bb2d
Kiarash Jamali <kjamali@mrc-lmb.cam.ac.uk>
2022-10-21 20:24:11 +0100

    Prune chains based on length

--- model_angelo/gnn/flood_fill.py
+++ model_angelo/gnn/flood_fill.py
@@ -144,16 +144,22 @@ def final_results_to_cif(
     all_atoms_np = all_atoms.numpy()
     chains = flood_fill(all_atoms_np, bfactors)
 
+    # Prune chains based on length
+    pruned_chains = [c for c in chains if len(c) > 3]
+
     chain_atom14_to_cif(
-        [aatype[c] for c in chains],
-        [all_atoms[c] for c in chains],
-        [atom_mask[c] for c in chains],
+        [aatype[c] for c in pruned_chains],
+        [all_atoms[c] for c in pruned_chains],
+        [atom_mask[c] for c in pruned_chains],
         cif_path,
-        bfactors=[bfactors[c] for c in chains],
+        bfactors=[bfactors[c] for c in pruned_chains],
     )
 
     chain_aa_logits = [
         final_results["aa_logits"][existence_mask][c] for c in chains
+    ]
+    pruned_chain_aa_logits = [
+        final_results["aa_logits"][existence_mask][c] for c in pruned_chains
     ]
     chain_hmm_confidence = [
         local_confidence_score_sigmoid(
@@ -166,7 +172,7 @@ def final_results_to_cif(
         hmm_dir_path = os.path.join(os.path.dirname(cif_path), "hmm_profiles")
         os.makedirs(hmm_dir_path, exist_ok=True)
 
-        for i, chain_aa_logits in enumerate(chain_aa_logits):
+        for i, chain_aa_logits in enumerate(pruned_chain_aa_logits):
             chain_name = number_to_chain_str(i)
             dump_aa_logits_to_hmm_file(
                 chain_aa_logits,
