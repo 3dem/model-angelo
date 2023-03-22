3b7555f29363580006d22667f615ac5d8b16465e
Kiarash Jamali <kjamali@mrc-lmb.cam.ac.uk>
2023-03-22 16:26:51 +0000

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -56,8 +56,8 @@ $python_exc setup.py install
 
 if [[ "${DOWNLOAD_WEIGHTS}" ]]; then
   echo "Writing weights to ${TORCH_HOME}"
-  $python_exc model_angelo/utils/setup_weights.py --bundle-name original
-  $python_exc model_angelo/utils/setup_weights.py --bundle-name original_no_seq
+  $python_exc model_angelo/utils/setup_weights.py --bundle-name nucleotides
+  $python_exc model_angelo/utils/setup_weights.py --bundle-name nucleotides_no_seq
 else
   echo "Did not download weights because the flag -w or --download-weights was not specified"
 fi
